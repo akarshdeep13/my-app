9bf4f5e9df44eb8d941f0b576afc3204f89e1a85
akarshdeep13 <akarshdeep@gmail.com>
2021-05-27 00:42:52 +0530

    Logo added

--- src/SurveyApp.js
+++ src/SurveyApp.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import WelcomePage from './WelcomePage';
-import './css/SurveyApp.css'
+import './css/SurveyApp.css';
+import logo from './images/logo.png';
 
 const SurveyApp = () => {
 	return (
 		<div>
 		<h2>Survey App</h2>
+		<img src={logo} className="App-logo" alt="logo" />
 		<p>Welcome user to Survey</p>
 		<WelcomePage/>
 		</div>
